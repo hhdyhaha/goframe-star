23f992984c1ae883a96ee324e5264e507de4ff5d
hhdyhaha <1164549585@qq.com>
2025-05-09 10:16:19 +0800

    更新 go.mod 和 go.sum 文件，升级依赖项并调整 Go 版本；在 main.go 中添加数据库连接检查功能

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"errors"
 	_ "goframe-star/internal/packed"
 
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+
 	"github.com/gogf/gf/v2/os/gctx"
+
+	"github.com/gogf/gf/v2/frame/g"
 
 	"goframe-star/internal/cmd"
 )
 
 func main() {
+	var err error
+
+	// 检查数据库是否能连接
+	err = connDb()
+	if err != nil {
+		panic(err)
+	}
+
 	cmd.Main.Run(gctx.GetInitCtx())
+}
+
+// connDb 检查数据库是否能连接
+func connDb() error {
+	err := g.DB().PingMaster()
+	if err != nil {
+		return errors.New("数据库连接失败，请检查配置")
+	}
+	return nil
 }
