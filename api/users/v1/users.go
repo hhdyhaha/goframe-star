1869988fed9a5d7b3f496e6234a14dbc0d275c7a
hhdyhaha <1164549585@qq.com>
2025-05-12 16:15:23 +0800

    更新用户注册请求结构体，添加中文描述和标签以增强可读性和可维护性

--- api/users/v1/users.go
+++ api/users/v1/users.go
@@ -3,10 +3,10 @@ package v1
 import "github.com/gogf/gf/v2/frame/g"
 
 type RegisterReq struct {
-	g.Meta   `path:"users/register" method:"POST"`
-	Username string `json:"username" v:"required|length:3,12"`
-	Password string `json:"password" v:"required|length:6,16"`
-	Email    string `json:"email" v:"required|email"`
+	g.Meta   `path:"users/register" method:"POST" sm:"注册" tags:"用户"`
+	Username string `json:"username" v:"required|length:3,12" dc:"用户名"`
+	Password string `json:"password" v:"required|length:6,16" dc:"密码"`
+	Email    string `json:"email" v:"required|email" dc:"邮箱"`
 }
 
 type RegisterRes struct {
